28e04ed95e9175c09e6570090265564cbce17fce
Francois Prunayre <fx.prunayre@gmail.com>
2019-01-11 14:46:48 +0100

    Update to GEMET 4.1.2 and remove INSPIRE related thesaurus which are now available through the INSPIRE Registry.

---
+++ gemet.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+./gemet-to-simpleskos.sh en
+./gemet-to-simpleskos.sh fr
+./gemet-to-simpleskos.sh it
+./gemet-to-simpleskos.sh nl
+./gemet-to-simpleskos.sh de
+./gemet-to-simpleskos.sh en fr it nl de
